de8b5ce8741b871aa1c3b21e7973dcc156387e9d
littlesekii <littlesekii@gmail.com>
2023-09-27 16:28:07 -0300

    Project started

---
+++ src/main/java/api/littlesekii/crudier/CrudierApplication.java
@@ -0,0 +1,13 @@
+package api.littlesekii.crudier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/littlesekii/crudier/CrudierApplicationTests.java
@@ -0,0 +1,13 @@
+package api.littlesekii.crudier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
